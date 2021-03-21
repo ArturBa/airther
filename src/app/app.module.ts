9362a893eeaf7f455a757615d207f8bf966c9254
Artur Bauer <bauerart1@gmail.com>
2021-03-21 12:22:14 +0100

    Add: PrimeNG components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeModule } from './prime.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    PrimeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
