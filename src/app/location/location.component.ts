49199b9d0212295a3e9d365f1724e4730dc144fb
wifilipiak <61492916+wifilipiak@users.noreply.github.com>
2021-03-29 14:26:45 +0200

    Fix location component

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OpenWeatherService } from 'src/app/services/open-weather.service';
+import { OpenWeatherService } from '../services/open-weather.service';
 
 @Component({
   selector: 'app-location',
@@ -7,9 +7,9 @@ import { OpenWeatherService } from 'src/app/services/open-weather.service';
   styleUrls: ['./location.component.scss'],
 })
 export class LocationComponent {
-  lat;
-  lon;
-  city = 's';
+  lat: number;
+  lon: number;
+  city: string;
 
   constructor(protected owService: OpenWeatherService) {}
 
@@ -21,23 +21,28 @@ export class LocationComponent {
         (position) => {
           this.lat = position.coords.latitude;
           this.lon = position.coords.longitude;
+          // TODO: remove when better display method available
           console.log(`position: ${this.lat} , ${this.lon}`);
         },
         (decline) => {
+          // TODO: show this to user
           console.log('Permission declined');
         }
       );
     } else {
+      // TODO: show this to user
       console.log('Geolocation is not supported');
     }
   }
 
   onSubmit(cityForm): void {
     this.city = cityForm.value.city;
+    // TODO: remove when better display method available
     console.log('City: ' + this.city);
     this.owService.getLocation(this.city).subscribe((location) => {
       this.lat = location[0].lat;
       this.lon = location[0].lon;
+      // TODO: remove when better display method available
       console.log(`position: ${this.lat} , ${this.lon}`);
     });
   }
