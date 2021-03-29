2dbefcc4dd37fa4b7579ffb8999c02eda2abbf17
wifilipiak <61492916+wifilipiak@users.noreply.github.com>
2021-03-29 14:32:18 +0200

    Fix too long URLs

--- src/app/services/URL.ts
+++ src/app/services/URL.ts
@@ -5,9 +5,11 @@ export const API_URL = {
     'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=' +
     APIkey,
   weather:
-    'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,daily,alerts&units=metric&appid=' +
+    'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}' +
+    '&exclude=minutely,daily,alerts&units=metric&appid=' +
     APIkey,
   air:
-    'http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat={lat}&lon={lon}&appid=' +
+    'http://api.openweathermap.org/data/2.5/air_pollution/forecast?' +
+    'lat={lat}&lon={lon}&appid=' +
     APIkey,
 };
