9e9b57654ebedcc2409997897a438d043136476d
BartlomiejWojcik <bartlomiej.wojcik153@gmail.com>
2021-03-30 21:57:50 +0200

    fix https

--- src/app/services/open-weather/URL.ts
+++ src/app/services/open-weather/URL.ts
@@ -5,14 +5,14 @@ import { environment } from '../../../environments/environment';
  */
 export const API_URL = {
   location:
-    'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=' +
+    'https://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=' +
     environment.weatherapi.apiKey,
   weather:
-    'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}' +
+    'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}' +
     '&exclude=minutely,daily,alerts&units=metric&appid=' +
     environment.weatherapi.apiKey,
   air:
-    'http://api.openweathermap.org/data/2.5/air_pollution/forecast?' +
+    'https://api.openweathermap.org/data/2.5/air_pollution/forecast?' +
     'lat={lat}&lon={lon}&appid=' +
     environment.weatherapi.apiKey,
 };
