c1fd1a2318d371f8c1fa534d6b27f78e16f5fef5
BartlomiejWojcik <bartlomiej.wojcik153@gmail.com>
2021-03-30 15:45:35 +0200

    Hide the weather api key

--- src/app/services/URL.ts
+++ src/app/services/URL.ts
@@ -1,15 +1,15 @@
-const APIkey = 'a993fb2adc4cfdf4377bba9ca8e5c1fd';
+import { environment } from 'src/environments/environment';
 
 export const API_URL = {
   location:
     'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=' +
-    APIkey,
+    environment.weatherapi.apiKey,
   weather:
     'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}' +
     '&exclude=minutely,daily,alerts&units=metric&appid=' +
-    APIkey,
+    environment.weatherapi.apiKey,
   air:
     'http://api.openweathermap.org/data/2.5/air_pollution/forecast?' +
     'lat={lat}&lon={lon}&appid=' +
-    APIkey,
+    environment.weatherapi.apiKey,
 };
